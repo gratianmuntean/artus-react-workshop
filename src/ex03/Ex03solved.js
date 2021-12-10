80ebff06f0034b518b1b02985b17f5ded572ef63
Muntean Gratian <muntean.gm@pg.com>
2021-12-10 17:40:21 +0200

    ex04

--- src/ex03/Ex03solved.js
+++ src/ex03/Ex03solved.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Ex03solved = () => {
+    const names = ['Mario', 'Jason', 'Joe', 'Joanna'];
+
     return (
         <>
             <h2>Ex03 solved</h2>
+            <ul>
+                {names?.map((name, i) => {
+                    return (
+                        <li key={i}>{name}</li>
+                    )
+                })}
+            </ul>
         </>
     );
 }
--- src/ex04/Ex04.js
+++ src/ex04/Ex04.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Ex04 = () => {
+    /*
+    Let's fetch some random data and display it in our component
+    We'll use this mock api endpoint https://jsonplaceholder.typicode.com/users
+    It retrieves a list of mock users. We want to do a basic get request as soon
+    as the page loads up, and display each user's name and email in a <li></li>
+    */
+
     return (
         <>
             <h2>Ex04</h2>
+            <ul>
+                {/* here you have to render the users */}
+            </ul>
         </>
     );
 }
