0e11e511677636352f48f19763fc6fdc5381fa63
Muntean Gratian <muntean.gm@pg.com>
2021-12-10 17:47:19 +0200

    ex04 solved + tbd ex05

--- src/ex04/Ex04solved.js
+++ src/ex04/Ex04solved.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Ex04solved = () => {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        return await fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json());
+    }
+
+    useEffect(() => {
+        fetchUsers().then(data => setUsers(data));
+    }, [])
+
     return (
         <>
             <h2>Ex04solved</h2>
+            <ul>
+                {users?.map((user, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{user?.name} | </span>
+                            <span>{user?.email}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </>
     );
 }
--- src/ex05/Ex05.js
+++ src/ex05/Ex05.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Ex05 = () => {
+    // use context request TBD
     return (
         <>
             <h2>Ex05</h2>
