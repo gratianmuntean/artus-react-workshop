b43ed031f8be9b168746c6afc26ad4587419103b
Muntean Gratian <muntean.gm@pg.com>
2021-12-10 17:36:23 +0200

    ex03

--- src/ex02/Ex02solved.js
+++ src/ex02/Ex02solved.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Ex02solved = () => {
+const Ex02 = () => {
+    // state
+    const [color, setColor] = useState('white');
+
+    // on change handler 
+    const onChangeColor = e => {
+        setColor(e?.target?.value);
+    }
+
     return (
         <>
-            <h2>Ex02solved</h2>
+            <h2>Ex02</h2>
+            <input type='text' onChange={onChangeColor} />
+            <br />
+            <div style={{
+                backgroundColor: color || 'white',
+                borderRadius: '50%',
+                width: 100,
+                height: 100
+            }}
+            >
+            </div>
         </>
     );
 }
 
-export default Ex02solved;
+export default Ex02;
--- src/ex03/Ex03.js
+++ src/ex03/Ex03.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Ex03 = () => {
+    /*
+    Let's see how we can display a list of components in react
+    You have an array of random names below, let's render them
+    in <li></li> elements inside the ul
+    */
+
+    // const names = ['Mario', 'Jason', 'Joe', 'Joanna'];
+
     return (
         <>
             <h2>Ex03</h2>
